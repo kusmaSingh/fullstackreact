eb0319fb4b24491ab4d4742f80989b7d2a7f6aea
Kusma Rani <kusma@insonix.com>
2018-04-16 18:23:06 +0530

    index.js file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-require('./services/passport')
-const authRoutes = require('./routes/authRoutes');
+require('./services/passport');
 const app = express();
-authRoutes(app);
+
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , ()=>{
