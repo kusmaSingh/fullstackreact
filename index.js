6c2825bf91adf108436fea369fe7c64000b06799
Kusma Rani <kusma@insonix.com>
2018-04-17 15:42:52 +0530

    cookies and session mentain

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
 require('./services/passport');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(keys.mongoLocalDbURI)
+    .then(() => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
 const app = express();
+// for cookie session .i.e 30 days
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , ()=>{
