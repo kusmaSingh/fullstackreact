e0d92ae969accb9d5049cc581026a4aa11b1f118
Kusma Rani <kusma@insonix.com>
2018-04-17 17:28:36 +0530

    add keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: "43247915554-12mao8bfmrensq116k7l5qb570tj5m5v.apps.googleusercontent.com",
+    googleClientSecret: "Agd68i_1KibBBtIPJ8cGXstp",
+    mongoURI: "mongodb://kusma:password@ds247479.mlab.com:47479/emaily-dev",
+    mongoLocalDbURI: "mongodb://localhost:27017/emaily-dev",
+    cookieKey: "hgdfuw123rhdl231kjhhjf45guefkjjjk234fhljkhhkjyuuyu"
+}
