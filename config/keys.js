23143d30d34e6d156cfd9864d094896a52a24147
Kusma Rani <kusma@insonix.com>
2018-04-17 18:01:01 +0530

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
-module.exports = {
-    googleClientID: "43247915554-12mao8bfmrensq116k7l5qb570tj5m5v.apps.googleusercontent.com",
-    googleClientSecret: "Agd68i_1KibBBtIPJ8cGXstp",
-    mongoURI: "mongodb://kusma:password@ds247479.mlab.com:47479/emaily-dev",
-    mongoLocalDbURI: "mongodb://localhost:27017/emaily-dev",
-    cookieKey: "hgdfuw123rhdl231kjhhjf45guefkjjjk234fhljkhhkjyuuyu"
+// figure out what set of credentails to return
+if(process.env.NODE_ENV === 'production'){
+    // we are in production envirnoment -return prod keys set
+    module.exports = require('./prod');
+}else
+{
+    module.exports = require('./prod');
+   //we are in developement envirnomet - return dev set of keys
 }
---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+
+}
+/* googleClientID: "732706155788-f1jp8h15qrd0bsj0m277eequmj2njv7o.apps.googleusercontent.com",
+    googleClientSecret: "QiI_676SIGjGfYvHD4YC4GZw",
+    mongoURI: "mongodb://kusma:password@ds247479.mlab.com:47479/emaily-dev",
+    mongoLocalDbURI: "mongodb://localhost:27017/emaily-dev",
+    cookieKey: "hgdfuw123rhdl231kjhhjf45guefkjjjk234fhljkhhkjyuuyu"*/
