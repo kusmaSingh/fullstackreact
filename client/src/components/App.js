ee770f6faf373b572b4594e4544f913235869570
Kusma Rani <kusma@insonix.com>
2018-04-19 17:17:57 +0530

    add stripe keys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions'  //  '*' means we will import all the actions and assign as object name actions
+import Header from './Header';
+import Landing from './Leading'
+//static component
+const Dashboard = () => <h2>Dashboard</h2>
+const Survey = () => <h2>Survey</h2>
 
 
-const App = () =>{
-    return(
-        <div>
-            Hi Kusma!
-        </div>
-    );
-};
 
-export default App;
+class App extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Route exact={true} path="/" component={Landing}/>
+                        <Route exact={true} path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={Survey}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import App from './components/App';
 import reducers from './reducers'
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 //const store  = createStore(() => [], {}, applyMiddleware());
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
